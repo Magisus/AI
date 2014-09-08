f9642be209fb6d95c1522bb3876cc392968ff69a
Maggie Dreyer <mdreyer@maclabcs7.local>
2014-09-08 14:49:01 -0700

    Fixed a warning

--- src/vacuum/StateAgent.java
+++ src/vacuum/StateAgent.java
@@ -3,33 +3,32 @@ package vacuum;
 import static edu.princeton.cs.introcs.StdRandom.uniform;
 
 public class StateAgent extends AbstractAgent {
-	
+
 	private int cleanSquares;
 	private Action current;
-	
-	public StateAgent(){
+
+	public StateAgent() {
 		cleanSquares = 0;
 		current = Action.SUCK;
 	}
 
 	@Override
 	public Action react(boolean dirty) {
-		if(dirty){
+		if (dirty) {
 			cleanSquares = 0;
 			return Action.SUCK;
-		} else {
-			cleanSquares++;
-			if(cleanSquares == 3){
-				current = selectAction();
-				cleanSquares = 0;
-			}
-			return current;
 		}
+		cleanSquares++;
+		if (cleanSquares == 3) {
+			current = selectAction();
+			cleanSquares = 0;
+		}
+		return current;
 	}
-	
-	private Action selectAction(){
+
+	private Action selectAction() {
 		int y = uniform(2);
-		if(current == Action.UP || current == Action.DOWN){
+		if (current == Action.UP || current == Action.DOWN) {
 			current = y == 0 ? Action.RIGHT : Action.LEFT;
 		} else {
 			current = y == 0 ? Action.UP : Action.DOWN;
