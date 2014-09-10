ac21d2fa484346a1b72ec0a29235dbe7980b1a80
Maggie Dreyer <mdreyer@maclabcs7.local>
2014-09-10 14:41:05 -0700

    Added searcher package

---
+++ src/search/BreadthFirstSearcher.java
@@ -0,0 +1,62 @@
+package search;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+public class BreadthFirstSearcher implements Searcher{
+
+	@Override
+	public void addToFrontier(Node node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean cutoff(Node node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean frontierIsEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getNodeCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initializeFrontier() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean inShallower(Node node, Map<String, Integer> map) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Node removeFromFrontier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void resetNodeCount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public LinkedList<Character> search(Node problem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
