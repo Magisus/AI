bdb412d6bec572953b5dc89861cb01f6db89586c
Maggie Dreyer <mdreyer@lclark.edu>
2014-09-17 14:24:00 -0700

    Added player implementations

---
+++ src/othello/AlphaBetaPlayer.java
@@ -0,0 +1,11 @@
+package othello;
+
+public class AlphaBetaPlayer implements Player{
+
+	@Override
+	public int move(State state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/othello/MinimaxPlayer.java
@@ -0,0 +1,11 @@
+package othello;
+
+public class MinimaxPlayer implements Player {
+
+	@Override
+	public int move(State state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
