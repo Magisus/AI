21e5cb719df59f3b1b4078247d26bb7aa14c61bc
billyv <billyv@lclark.edu>
2014-09-17 14:50:44 -0700

    starting work on minimax

--- src/othello/MinimaxPlayer.java
+++ src/othello/MinimaxPlayer.java
@@ -1,11 +1,33 @@
 package othello;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class MinimaxPlayer implements Player {
 
+	int maxSearchDepth;
+	
+	
 	@Override
 	public int move(State state) {
-		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public int move(State state, int depth){
+		List<Integer> legMoves = state.legalMoves();
+		if (legMoves.get(0) == State.PASS){
+			return State.PASS;
+		}
+		if (depth == maxSearchDepth ){
+			Collections.sort(legMoves);
+			return legMoves.get(legMoves.size() - 1);
+		}
+		return 0;
+	}
+	
+	public MinimaxPlayer(int maxSearchDepth) {
+		this.maxSearchDepth = maxSearchDepth;
 	}
 
 }
