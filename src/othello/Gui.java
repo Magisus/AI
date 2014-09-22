f5094b28561f5b94e8d73c7d2406443627386529
billyv <billyv@lclark.edu>
2014-09-22 15:28:31 -0700

    code for running tourney and saving data to file

--- src/othello/Gui.java
+++ src/othello/Gui.java
@@ -1,29 +1,78 @@
 package othello;
 
 import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import static edu.princeton.cs.introcs.StdDraw.*;
 import static othello.State.*;
 
 /** Graphic user interface for Othello. */
-public class Gui {
+public class Gui {	
 
+	//print mini 1 vs alpha 2 
 	public static void main(String[] args) {
-		for (int depth1 = 1; depth1 <= 7; depth1++) {
-			for (int depth2 = 1; depth2 <= 7; depth2++) {
-				System.out.println(new Gui().run(new AlphaBetaPlayer(depth1, 'X'), new MinimaxPlayer(depth2, 'O')));
-				System.out.println(new Gui().run(new MinimaxPlayer(depth1, 'X'), new AlphaBetaPlayer(depth2, 'O')));
-				System.out.println();
+		
+		StringBuilder node1 = new StringBuilder();
+		StringBuilder node2 = new StringBuilder();
+		StringBuilder winner = new StringBuilder();
+		
+		//run tourney for Minimax as X and Alpha as O, then Minimax as X and Minimax as O
+		for (int depth1 = 1; depth1 <= 3; depth1++) {
+			for (int i = 0; i < 2; i++){
+				if (i == 0 ) {
+					for (int depth2 = 1; depth2 <= 3; depth2++) {
+						new Gui().run(new MinimaxPlayer(depth1, 'X'), new AlphaBetaPlayer(depth2, 'O'), node1, node2, winner);
+					}
+				}
+				if (i == 1 ) {
+					for (int depth2 = 1; depth2 <= 3; depth2++) {
+						new Gui().run(new MinimaxPlayer(depth1, 'X'), new MinimaxPlayer(depth2, 'O'), node1, node2, winner);
+					}
+				}
 			}
+			node1.append("\n"); node2.append("\n"); winner.append("\n");
 		}
+		
+		//run tourney for Alpha as X and Minimax as O, then Alpha as O and Alpha as X
+		for (int depth1 = 1; depth1 <= 3; depth1++) {
+			for (int i = 0; i < 2; i++){
+				if (i == 0 ) {
+					for (int depth2 = 1; depth2 <= 3; depth2++) {
+						new Gui().run(new AlphaBetaPlayer(depth1, 'X'), new MinimaxPlayer(depth2, 'O'), node1, node2, winner);
+					}
+				}
+				if (i == 1 ) {
+					for (int depth2 = 1; depth2 <= 3; depth2++) {
+						new Gui().run(new AlphaBetaPlayer(depth1, 'X'), new AlphaBetaPlayer(depth2, 'O'), node1, node2, winner);
+					}
+				}
+			}
+			node1.append("\n"); node2.append("\n"); winner.append("\n");
+		}
+		
+		//print results
+		try(PrintWriter writer = new PrintWriter("Node1.csv")){
+			writer.print(node1);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try(PrintWriter writer = new PrintWriter("Node2.csv")){
+			writer.print(node2);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try(PrintWriter writer = new PrintWriter("Winner.csv")){
+			writer.print(winner);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	/** Plays the game. */
-	public String run(Player player1, Player player2) {
+	public String run(Player player1, Player player2, StringBuilder node1, StringBuilder node2, StringBuilder winner) {
 //		show(0);
-		System.out.println("Player 1: " + player1.getClass().getName()
-				+ " at depth " + player1.getMaxSearchDepth());
-		System.out.println("Player 2: " + player2.getClass().getName()
-				+ " at depth " + player2.getMaxSearchDepth());
 		State board = new State();
 		while (!board.gameOver()) {
 			int move;
@@ -40,14 +89,17 @@ public class Gui {
 //				draw(board, "Illegal move.", 1000);
 			}
 		}
-		System.out.println("Player 1 node count: " + player1.getNodeCount()
-				+ "  Player 2 node count: " + player2.getNodeCount());
+		node1.append(player1.getNodeCount() + ", ");
+		node2.append(player2.getNodeCount() + ", ");
 		String result = "Game over. ";
 		if (board.score() > 0) {
+			winner.append("true, ");
 			result += "Black wins!";
 		} else if (board.score() < 0) {
+			winner.append("false, ");
 			result += "White wins!";
 		} else {
+			winner.append("tie, ");
 			result += "Tie";
 		}
 		return result;
