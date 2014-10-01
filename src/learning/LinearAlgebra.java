535c984a665ad72d2f8c38f65816aab93dc77da4
James Fischer <jamesfischer@lclark.edu>
2014-10-01 14:52:56 -0700

    Wrote dotProduct and sum in LinearAlgebra

--- src/learning/LinearAlgebra.java
+++ src/learning/LinearAlgebra.java
@@ -2,17 +2,24 @@ package learning;
 
 public class LinearAlgebra {
 
-		public static double dotProduct(double[] a, double[] b) {
-			double result = 0;
-			return result;
+	public static double dotProduct(double[] a, double[] b) {
+		double result = 0;
+		for (int i = 0; i < a.length; i++) {
+			result += a[i] * b[i];
 		}
-	
-		public static double[] sum(double[] a, double[] b) {
-			return new double[1];
+		return result;
+	}
+
+	public static double[] sum(double[] a, double[] b) {
+		double sum[] = new double[a.length];
+		for (int i = 0; i < a.length; i++) {
+			sum[i] = a[i] + b[i];
 		}
-		
-		public static double[] scale(double a, double[] b) {
-			return new double[1];
-		}
-		
+		return new double[1];
+	}
+
+	public static double[] scale(double a, double[] b) {
+		return new double[1];
+	}
+
 }
