e626ea6815b8862dd5a40ee47a168509ffb8f366
Maggie Dreyer <mdreyer@lclark.edu>
2014-10-24 13:43:02 -0700

    Started adding some method stubs

--- src/neural/Network.java
+++ src/neural/Network.java
@@ -1,13 +1,25 @@
 package neural;
 
 public class Network {
+	
+	private AbstractNeuron[] in;
+	private AbstractNeuron[] hid;
+	private AbstractNeuron[] out;
 
-	public Network(int i, int j, int k) {
-		// TODO Auto-generated constructor stub
+	public Network(int in, int hid, int out) {
+		this.in = new AbstractNeuron[in];
+		this.hid = new AbstractNeuron[hid];
+		this.out = new AbstractNeuron[out];
 	}
 
-	public void train(double[][] inputs, double[][] correct, int i) {
-		// TODO Auto-generated method stub
+	public void train(double[][] inputs, double[][] correct){
+		
+	}
+	
+	public void train(double[][] inputs, double[][] correct, int epochs) {
+		for(int i = 0; i < epochs; i++){
+			train(inputs, correct);
+		}
 		
 	}
 
@@ -17,9 +29,14 @@ public class Network {
 		
 	}
 
-	public AbstractNeuron getNeuron(int i, int i2) {
-		// TODO Auto-generated method stub
-		return null;
+	public AbstractNeuron getNeuron(int layer, int index) {
+		if(layer == 0){
+			return in[index];
+		} else if(layer == 1){
+			return hid[index];
+		} else{
+			return out[index];
+		}
 	}
 
 }
--- src/neural/SigmoidNeuron.java
+++ src/neural/SigmoidNeuron.java
@@ -1,15 +1,28 @@
 package neural;
 
 public class SigmoidNeuron extends AbstractNeuron{
+	
+	private double delta;
 
 	public SigmoidNeuron(InputNeuron[] neurons){
 		super(neurons, null, 0);
 	}
+	
+	double getDelta(){
+		return delta;
+	}
+	
+	void setDelta(double delta){
+		this.delta = delta;
+	}
 
 	@Override
 	double squash(double sum) {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.pow(Math.E, sum) / (1 + Math.pow(Math.E, sum));
+	}
+	
+	public void updateWeights(){
+		//use delta
 	}
 	
 }
